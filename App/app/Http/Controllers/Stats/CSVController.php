084f6894424a8758a8b70829e08eae802df84375
Fire <w21017968@northumbria.ac.uk>
2024-03-22 20:31:06 +0000

    Changes listed in desc
    -CHanges routing and added auth middleware
    -Got User ID into forms
    -Retrieving more data and passing user id

--- App/app/Http/Controllers/Stats/CSVController.php
+++ App/app/Http/Controllers/Stats/CSVController.php
@@ -4,6 +4,7 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\User;
 
 class CSVController extends Controller {
     public function getEvents(Request $request) {
@@ -62,18 +63,42 @@ class CSVController extends Controller {
             ])->deleteFileAfterSend(true);
     }
 
+
+    public function getAllUsers(Request $request) {
+        $users=User::all();
+        $csvData=[ ['User ID, User Name, User Email']];
+
+        foreach ($users as $user) {
+            $csvData[]=[$user->_id,
+            $user->name,
+            $user->email];
+        }
+
+        // Create CSV file
+        $csvFileName='userList.csv';
+        $filePath=storage_path('app/'. $csvFileName);
+        $file=fopen($filePath, 'w');
+
+        foreach ($csvData as $line) {
+            fputcsv($file, $line);
+        }
+
+        fclose($file);
+
+        // Download CSV file
+        return Response::download($filePath, $csvFileName, [ 'Content-Type'=> 'text/csv',
+            ])->deleteFileAfterSend(true);
+    }
+
+
     public function getRegisterUsersOnEvent (Request $request) {
 
 
-        
-        // Generate CSV content
-        $csvData=[ ['Name',
-        'Email'],
-        ['John Doe',
-        $request->eventId],
-        ['Jane Smith',
-        $request->id],
-        // Add more data as needed
+       // Generate CSV content
+       $csvData=[ ['Event id',
+       'User id'],
+       [$request->eventId,
+       $request->id],
         ];
 
         // Create CSV file
