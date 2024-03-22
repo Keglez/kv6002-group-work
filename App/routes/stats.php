b051a5cdc85dda233508676e692ed2a7896968d3
Fire <w21017968@northumbria.ac.uk>
2024-03-22 19:38:39 +0000

    Changing Routing
    This was done just to clean up a directory and make it easier to understand.

--- App/routes/stats.php
+++ App/routes/stats.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/sl', [StatController::class, 'create'])-> name('StatList');
-Route::get('/sl/EventPercentGraph', [StatController::class, 'ePerGraph'])-> name('eventPercentGraph');
+Route::get('/sl/EventPercentGraph', [StatController::class, 'eventPercentageGraph'])-> name('eventPercentGraph');
 Route::get('/sl/UserDemoGraph', [StatController::class, 'userDemoGraph'])-> name('userDemoGraph');
 Route::get('/sl/ExternalDataGraph', [StatController::class, 'externalDataGraph'])-> name('externalDataGraph');
 Route::get('/sl/CSV//uData{id}', [CSVController::class, 'getUserData'])-> name("userdata");
