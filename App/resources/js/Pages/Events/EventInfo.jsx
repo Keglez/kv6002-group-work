63de960122d94b637a18318888bd629295f02407
Keegan Hinnigan <117363682+Keglez@users.noreply.github.com>
2024-03-24 22:31:12 +0000

    Update

--- App/resources/js/Pages/Events/EventInfo.jsx
+++ App/resources/js/Pages/Events/EventInfo.jsx
@@ -118,7 +118,7 @@ export default function EventInfo({ auth, event })
                                 </div>
                                 <div>
                                     <span><a
-                                                href="message"
+                                                href={route("chats")}
                                                 className="px-4 py-2 bg-red-200 rounded border border-gray-500 hover:bg-red-500 focus:outline-none focus:border-gray-600 focus:ring focus:ring-gray-500 focus:ring-opacity-50"
                                             >
                                             Event Chat</a></span>
--- App/routes/chats.php
+++ App/routes/chats.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Messages\ChatController;
 
-Route::get('/message', [ChatController::class, 'messageDash']) ->middleware(['auth', 'verified'])->name('chats');;
-Route::get('/event/message', [ChatController::class, 'messageDash']) ->middleware(['auth', 'verified'])->name('chats');;
-Route::post('/message/send', [ChatController::class, 'storeChatData']) ->middleware(['auth', 'verified'])->name('send_chat');;
+Route::get('/message', [ChatController::class, 'messageDash']) ->middleware(['auth', 'verified'])->name('chats');
+Route::get('/event/message', [ChatController::class, 'messageDash']) ->middleware(['auth', 'verified'])->name('event.chat');
+Route::post('/message/send', [ChatController::class, 'storeChatData']) ->middleware(['auth', 'verified'])->name('send_chat');
